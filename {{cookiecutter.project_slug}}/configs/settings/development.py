608b8d343e4eec5ab786a7576f11bada42279f56
cla-bit <doublep098@gmail.com>
2026-06-28 15:29:45 +0100

    fix: added logic to use postgresql settings

--- {{cookiecutter.project_slug}}/configs/settings/development.py
+++ {{cookiecutter.project_slug}}/configs/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3c===wj5z(m(aciz*fcf#@!#tczs+qu#px(nolq@(=y8d3ewf_'
+SECRET_KEY = '{{ cookiecutter.django_secret_key }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -12,13 +12,28 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+{% if cookiecutter.use_postgresql == "yes" %}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": os.getenv("POSTGRES_SSLMODE", "require"),
+        },
+    }
+}
+{% else %}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+{% endif %}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # For development
